059510499c85e6d1699703cac883621a5f854a1c
Eser Ozvataf <eser@ozvataf.com>
2022-10-06 04:26:50 +0300

    added sentry integration.

--- src/app.ts
+++ src/app.ts
@@ -3,15 +3,18 @@ import { jwtMiddleware } from "@hex/service/middlewares/jwt.ts";
 import { homeAction } from "@app/actions/home.ts";
 import { echoAction } from "@app/actions/echo.ts";
 import { errorProneAction } from "@app/actions/error-prone.ts";
+import * as Sentry from "npm:@sentry/node";
 
 interface AppOptions extends ServiceOptions {
   mongoDbConnString?: string;
+  sentryDsn?: string;
 }
 
 const app = run<AppOptions>(async (s) => {
   // configure options
   await s.configureOptions((env, options) => {
     options.mongoDbConnString = env.readString("MONGODB_CONNSTRING");
+    options.sentryDsn = env.readString("SENTRY_DSN");
   });
 
   // configure di registry
@@ -34,6 +37,18 @@ const app = run<AppOptions>(async (s) => {
 
     return echoAction(ctx?.params?.slug);
   });
+
+  // sentry initialization
+  if (s.options.sentryDsn !== undefined) {
+    Sentry.init({
+      dsn: s.options.sentryDsn,
+
+      // Set tracesSampleRate to 1.0 to capture 100%
+      // of transactions for performance monitoring.
+      // We recommend adjusting this value in production
+      tracesSampleRate: 1.0,
+    });
+  }
 });
 
 export { app, app as default };
